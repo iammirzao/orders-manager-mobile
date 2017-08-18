bcf166414ddbfe09a3605aa2b7fd227383a24934
Mirza Omerovic <mirza.omerovic@hpe.com>
2017-08-18 09:30:52 +0200

    log out functionality

--- www/js/controllers/main/OrdersManagerCtrl.js
+++ www/js/controllers/main/OrdersManagerCtrl.js
@@ -1,10 +1,15 @@
 angular.module('app.ordersmanager', [])
 
-.controller('ordersManagerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('ordersManagerCtrl', ['$scope', '$stateParams', 'UserService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, UserService) {
 
+  console.log('Orders Manager Controller Initialized');
+
+  $scope.logOut = function (){
+    UserService.logOut();
+  }
 
 }])
 
