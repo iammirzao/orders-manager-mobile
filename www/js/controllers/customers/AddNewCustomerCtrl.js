e8cdc61626780963153c0930ce95c0314587a14f
Mirza Omerovic <mirza.omerovic@hpe.com>
2017-08-18 09:37:34 +0200

    add new order and customer

--- www/js/controllers/customers/AddNewCustomerCtrl.js
+++ www/js/controllers/customers/AddNewCustomerCtrl.js
@@ -1,9 +1,21 @@
 angular.module('app.addnewcustomer', [])
 
-.controller('addNewCustomerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('addNewCustomerCtrl', ['$scope', '$stateParams', 'OrdersAndCustomersService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, OrdersAndCustomersService) {
 
+  $scope.customer = {
+    fullName: "Mirza",
+    address: "Samira Gradisica 5",
+    city: "Vogosca",
+    zip: 71320,
+    country: "BA",
+    contactNo: 38761318717
+  };
+
+  $scope.addCustomer = function (customer){
+    OrdersAndCustomersService.addCustomer(customer);
+  }
 
 }])
--- www/js/controllers/orders/AddNewOrderCtrl.js
+++ www/js/controllers/orders/AddNewOrderCtrl.js
@@ -1,9 +1,18 @@
 angular.module('app.addneworder', [])
 
-.controller('addNewOrderCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('addNewOrderCtrl', ['$scope', '$stateParams', 'OrdersAndCustomersService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, OrdersAndCustomersService) {
 
+  $scope.order = {
+    customer: "",
+    orderInfo: "",
+    trackingNo: ""
+  };
+
+  $scope.addOrder = function (order){
+    OrdersAndCustomersService.addOrder(order);
+  }
 
 }])
