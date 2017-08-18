2073bf159e93cdeb06420415a6027306f075a5f7
Mirza Omerovic <mirza.omerovic@hpe.com>
2017-08-18 09:28:27 +0200

    update services (user service, orders and customers service)

--- www/js/services/orderscustomers/OrdersAndCustomersService.js
+++ www/js/services/orderscustomers/OrdersAndCustomersService.js
@@ -1,26 +1,87 @@
 angular.module('app.orderscustomers', [])
 
-.factory('OrdersAndCustomersService', ['$state', '$firebase', '$rootScope',
+.factory('OrdersAndCustomersService', ['$state', '$firebase', '$rootScope', 'UserService',
 
-function($state, $firebase, $rootScope){
-  var database = firebase.database();
+function($state, $firebase, $rootScope, UserService){
+
+  var orderInfo = {};
+  var customerInfo = {};
+
   return {
-    addCustomer: function(user) {
-      firebase.database().ref('users/' + userId).set({
-        creator: user.name,
-        customerName: user.customer,
-        customerEmail: user.email,
-        customerNumber: user.email,
-        customerAddress: user.email,
-        customerEmail: user.email,
-        profile_picture : imageUrl
+    addOrder: function (order){
+      var ref = firebase.database().ref().push();
+      var id = ref.key;
+      firebase.database().ref('orders/' + id).set({
+        creator: UserService.getCurrentUser().uid,
+        customer: order.customer,
+        orderInfo: order.orderInfo,
+        trackingNo: order.trackingNo,
+        id: id
       });
-      console.log("Customer is successfully added");
+      console.log("Order is successfully added: ", id);
+      $state.go('ordersManager.orders');
     },
 
-    signIn: function (user){
-      console.log("test2");
-    }
+    updateOrder: function (order){
+      firebase.database().ref('orders/' + order.id).set({
+        creator: UserService.getCurrentUser().uid,
+        customer: order.customer,
+        orderInfo: order.orderInfo,
+        trackingNo: order.trackingNo,
+        id: order.id
+      });
+      console.log("Order is successfully updated: ", order.id);
+      $state.go('ordersManager.orders');
+    },
+
+    addCustomer: function(customer) {
+      var database = firebase.database();
+      console.log("BAZA: ", database);
+      var ref = firebase.database().ref().push();
+      var id = ref.key;
+      firebase.database().ref('customers/' + id).set({
+        creator: UserService.getCurrentUser().uid,
+        fullName: customer.fullName,
+        address: customer.address,
+        city: customer.city,
+        zip: customer.zip,
+        country: customer.country,
+        contactNo: customer.contactNo,
+        id: id
+      });
+      console.log("Customer is successfully added");
+      $state.go('ordersManager.customers');
+    },
+
+    setOrderInfo: function (order){
+      orderInfo = order;
+    },
+
+    getOrderInfo: function (){
+      return orderInfo;
+    },
+
+    setCustomerInfo: function (customer){
+      customerInfo = customer;
+    },
+
+    getCustomerInfo: function (){
+      return customerInfo;
+    },
+
+//    getOrders: function() {
+//      var getOrders = firebase.database().ref('orders/');
+//      getOrders.on('value', function(snapshot) {
+//       var allOrders = snapshot.val();
+//       var orders = [];
+//       _.forEach(allOrders, function(value) {
+//         if(value.creator == UserService.getCurrentUser().uid){
+//           orders.push(value);
+//         }
+//       });
+//      });
+//    }
+
   };
 
 }]);
--- www/js/services/user/UserService.js
+++ www/js/services/user/UserService.js
@@ -7,69 +7,86 @@ function($state, $firebase, $rootScope){
   return {
     signUp: function(user) {
       firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
-          .then(function (){
-            console.log("Sign up successful!");
-            $state.go('ordersManager.login');
-          })
-          .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode === 'auth/weak-password') {
-              alert('The password is too weak.');
-            }
-            else{
-              alert(errorMessage);
-            }
-          });
-    },
-
-    signIn: function (user){
-        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-          .then(function (user){
-           console.log("Sign in successful!: ", user);
-           $rootScope.user = user;
-           $state.go('ordersManager.orders');
-         })
-         .catch(function(error) {
+        .then(function (){
+          console.log("Sign up successful!");
+          $state.go('ordersManager.login');
+        })
+        .catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-          if (errorCode === 'auth/wrong-password') {
-            alert('Wrong password.');
+          if (errorCode === 'auth/weak-password') {
+            alert('The password is too weak.');
           }
           else{
             alert(errorMessage);
           }
         });
-      },
+    },
 
-      isLoggedIn: function(){
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            return user;
+    signIn: function (user){
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(function (user){
+         console.log("Sign in successfull: ", user);
+         $state.go('ordersManager.orders');
+        })
+        .catch(function(error) {
+        // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
           }
-          else {
-            // No user is signed in.
-            console.log("No user is signed in!");
+         else{
+            alert(errorMessage);
           }
         });
-      },
+    },
 
-      updateUser: function (name, newemail) {
-        $rootScope.currentUser = firebase.auth().currentUser;
-        $rootScope.currentUser.updateProfile({
-          displayName: name
-        }).then(function() {
-            if(newemail != $rootScope.currentUser.email){
-              $rootScope.currentUser.updateEmail(newemail);
-            }
-            console.log("User is updated successful!", $rootScope.currentUser);
-        }).catch(function(error) {
+    logOut: function (){
+      firebase.auth().signOut()
+        .then(function (response){
+         console.log("Sign out successfull!");
+         $state.go('ordersManager.login');
+        })
+        .catch(function(error) {
           // An error happened.
           alert(error.message);
         });
-      }
+    },
+
+    getCurrentUser: function(){
+      var user = firebase.auth().currentUser;
+      return user;
+    },
+
+//    isLoggedIn: function(){
+//      firebase.auth().onAuthStateChanged(function(user) {
+//        if (user) {
+//          return user;
+//        }
+//        else {
+//          // No user is signed in.
+//          console.log("No user is signed in!");
+//        }
+//      });
+//    },
+
+    updateUser: function (name, newemail) {
+      var user = firebase.auth().currentUser;
+      user.updateProfile({
+        displayName: name
+      }).then(function() {
+          if(newemail != "" && newemail != user.email){
+            user.updateEmail(newemail);
+          }
+          console.log("User is updated successful!", user);
+          $state.go('ordersManager.orders');
+      }).catch(function(error) {
+        // An error happened.
+        alert(error.message);
+      });
+    }
   };
 
 }]);
