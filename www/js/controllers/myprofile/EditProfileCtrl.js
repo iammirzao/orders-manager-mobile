d8978b954bcdbab30ffc1767bfd93c4f13e65223
Mirza Omerovic <mirza.omerovic@hpe.com>
2017-08-18 09:33:22 +0200

    edit profile

--- www/js/controllers/myprofile/EditProfileCtrl.js
+++ www/js/controllers/myprofile/EditProfileCtrl.js
@@ -1,9 +1,22 @@
 angular.module('app.editprofile', [])
 
-.controller('editProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('editProfileCtrl', ['$scope', '$stateParams', 'UserService', '$rootScope', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, UserService, $rootScope) {
 
+  $scope.init = function () {
+     var currentUser = UserService.getCurrentUser();
+     $scope.user = {
+        displayName: currentUser.displayName,
+        email: currentUser.email
+     }
+  };
+
+  $scope.updateUser = function (user){
+    UserService.updateUser(user.displayName, user.email);
+  }
+
+  $scope.init();
 
 }])
